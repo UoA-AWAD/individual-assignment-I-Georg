47657a92a612f3fbb39f5bfaa0984f471f0153e8
I-Georg <i.georgieva.18@aberdeen.ac.uk>
2019-11-09 11:53:09 +0000

    installed bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ require("jquery")
 
 import Highcharts from 'highcharts',
 window.Highcharts = Highcharts;
+import 'bootstrap'
+import './src/application.scss'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
